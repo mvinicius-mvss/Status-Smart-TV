569641de3189f472a44ed62529a86a0f7940967a
Marcos Vinicius <vinicius-mvss@live.com>
2023-02-17 15:26:34 -0300

    Treinando Métodos

---
+++ src/Usuario.java
@@ -0,0 +1,11 @@
+public class Usuario {
+    public static void main (String[] args){
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println(smartTv.ligada);
+        System.out.println(smartTv.canal);
+        System.out.println(smartTv.volume);
+
+
+    }
+}
