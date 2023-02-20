fb3c7a5495f5f356ec2aee3432d5ce4b31a2b0fb
Marcos Vinicius <vinicius-mvss@live.com>
2023-02-19 23:45:41 -0300

    Colocar testes na p/ testar a interação do método

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,11 +1,30 @@
 public class Usuario {
     public static void main (String[] args){
         SmartTv smartTv = new SmartTv();
-
-        System.out.println(smartTv.ligada);
-        System.out.println(smartTv.canal);
-        System.out.println(smartTv.volume);
-
+        // status inicial da tv
+        System.out.println("Tv está ligada? " + smartTv.ligada);
+        System.out.println("A tv está no canal: " +smartTv.canal);
+        System.out.println("A tv está no volume: " +smartTv.volume);
+        
+        smartTv.ligar(); // ligou a tv
+        smartTv.aumentarVolume(); 
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.mudarCanal(17);
+        
+        
+        
+        
+        // status da tv após os testes
+        System.out.println("Tv está ligada? " + smartTv.ligada);
+        System.out.println("A tv está no canal: " +smartTv.canal);
+        System.out.println("A tv está no volume: " +smartTv.volume);
 
     }
 }
